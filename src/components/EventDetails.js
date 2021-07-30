14d95d94d4c0d7b428900de1ca075cd998ceb8f3
Attila Béli <thirdeye0@gmail.com>
2021-07-30 17:18:00 +0200

    even now contains relevant rides

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,5 +1,5 @@
+import React from "react";
 import { Row, Col } from "antd";
-import { useState } from "react";
 import { useParams } from "react-router";
 import useFirestore from "../useFirestore";
 import RidesTable from "./RidesTable";
@@ -7,28 +7,33 @@ import RidesTable from "./RidesTable";
 const EventDetails = () => {
   const { id } = useParams();
   const event = useFirestore("events", `${id}`);
-  const hasRides = event.data !== null && event.data.rides.length !== 0
-
+  const hasRides = event?.data?.rides?.length !== 0 ? true : false;
 
   return (
     <>
       {event.data !== null && (
-        <Row>
-          <Col span={24} style={{ textAlign: "center" }}>
-            <h2>{event.data.name}</h2>
-          </Col>
-        </Row>
+        <React.Fragment>
+          <Row>
+            <Col span={24} style={{ textAlign: "center" }}>
+              <h2>{event.data.name}</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24} style={{ textAlign: "center" }}>
+              <h4>Felajánlott helyek</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24} style={{ textAlign: "center" }}>
+              {hasRides ? (
+                <RidesTable eventID={id} />
+              ) : (
+                <p>Nincs még felajánlott szabad hely!</p>
+              )}
+            </Col>
+          </Row>
+        </React.Fragment>
       )}
-      <Row>
-        <Col span={24} style={{ textAlign: "center" }}>
-          <h4>Felajánlott helyek</h4>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24} style={{ textAlign: "center" }}>
-          {hasRides ? <RidesTable rides={event.data.rides} /> : <p>Nincs még felajánlott szabad hely!</p>}
-        </Col>
-      </Row>
     </>
   );
 };
--- src/components/RidesTable.js
+++ src/components/RidesTable.js
@@ -1,87 +1,102 @@
 import { Row, Col } from "antd";
 import { Table, Tag } from "antd";
-import useFirestore from "../useFirestore";
+import { useEffect, useState } from "react";
+import db from "../db";
 
 const RidesTable = (props) => {
-  const { rides } = props;
+  const { eventID } = props;
+  const [data, setData] = useState([]);
 
-  // console.log(rides);
+  useEffect(() => {
+    db.collection("rides")
+      .where("eventID", "==", `${eventID}`)
+      .get()
+      .then((querySnapshot) => {
+        let temp = [];
+        querySnapshot.forEach((doc) => {
+          temp.push(doc.data());
+        });
+        setData(
+          temp.map((ride, i) => ({
+            key: i,
+            name: ride.driverNickname,
+            car: ride.carDetails.type,
+            space: ride.maxSpace,
+            tags: ride.tags.map((tag) => tag),
+          }))
+        );
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }, [eventID]);
 
-  // const data = rides.data.map((ride, i) => ({
-  //   key: i,
-  //   name: ride.driverNickname,
-  //   car: ride.carDetails.type,
-  //   space: ride.maxSpace,
-  //   tags: ride.tags.map((tag) => tag),
-
-  // }));
-
-  // const columns = [
-  //   {
-  //     title: "Sofőr",
-  //     dataIndex: "name",
-  //     key: "name",
-  //     render: (text) => text,
-  //   },
-  //   {
-  //     title: "Jármű",
-  //     dataIndex: "car",
-  //     key: "car",
-  //   },
-  //   {
-  //     title: "Szabad helyek száma",
-  //     dataIndex: "space",
-  //     key: "address",
-  //   },
-  //   {
-  //     title: "Címkék",
-  //     key: "tags",
-  //     dataIndex: "tags",
-  //     render: (tags) => (
-  //       <>
-  //         {tags.map((tag) => {
-  //           let color = "";
-  //           if (tag === "ac") {
-  //             color = "blue";
-  //           }
-  //           if (tag === "non-smoking") {
-  //             color = "red";
-  //           }
-  //           if (tag === "highway") {
-  //             color = "orange";
-  //           }
-  //           return (
-  //             <Tag color={color} key={tag}>
-  //               {tag.toUpperCase()}
-  //             </Tag>
-  //           );
-  //         })}
-  //       </>
-  //     ),
-  //   },
-  //   // {
-  //   //   title: "Műveletek",
-  //   //   key: "action",
-  //   //   render: (text, record) => (
-  //   //     <Space size="middle">
-  //   //       <a>Szerkesztés</a>
-  //   //       <a>Törlés</a>
-  //   //     </Space>
-  //   //   ),
-  //   // },
-  // ];
+  const columns = [
+    {
+      title: "Sofőr",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => text,
+    },
+    {
+      title: "Jármű",
+      dataIndex: "car",
+      key: "car",
+    },
+    {
+      title: "Szabad helyek száma",
+      dataIndex: "space",
+      key: "address",
+    },
+    {
+      title: "Címkék",
+      key: "tags",
+      dataIndex: "tags",
+      render: (tags) => (
+        <>
+          {tags.map((tag) => {
+            let color = "";
+            if (tag === "ac") {
+              color = "blue";
+            }
+            if (tag === "non-smoking") {
+              color = "red";
+            }
+            if (tag === "highway") {
+              color = "orange";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    // {
+    //   title: "Műveletek",
+    //   key: "action",
+    //   render: (text, record) => (
+    //     <Space size="middle">
+    //       <a>Szerkesztés</a>
+    //       <a>Törlés</a>
+    //     </Space>
+    //   ),
+    // },
+  ];
 
   return (
     <>
       <Row>
         <Col span={24}>
-          {/* <Table
+          <Table
             columns={columns}
             dataSource={data}
             //pagination is not where it supposed to - check API
             pagination={{ position: "bottomCenter" }}
             style={{ width: "100%" }}
-          /> */}
+          />
         </Col>
       </Row>
     </>
