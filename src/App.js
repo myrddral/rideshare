3d412537fbd61a3a6892fff40727b1cb9e8d54eb
Attila Béli <thirdeye0@gmail.com>
2021-08-25 11:32:10 +0200

    locale changed to hungarian

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import RideShare from "./components/RideShare";
 import { AuthProvider } from "./contexts/AuthContext";
+import { ConfigProvider } from 'antd';
+import huHU from 'antd/lib/locale/hu_HU';
 
 function App() {
   return (
     <AuthProvider>
+      <ConfigProvider locale={huHU}>
       <RideShare />
+      </ConfigProvider>
     </AuthProvider>
   );
 }
